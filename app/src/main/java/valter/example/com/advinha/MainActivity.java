e5d720c8ba2a7a9162e637fc0fafcecc0bd84705
Valter <valtinholima@gmail.com>
2017-05-25 01:10:36 -0300

    appfinalizado

--- app/src/main/java/valter/example/com/advinha/MainActivity.java
+++ app/src/main/java/valter/example/com/advinha/MainActivity.java
@@ -2,21 +2,35 @@ package valter.example.com.advinha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+
     private Button botaoJogar;
     private TextView textoResultado;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        botaoJogar = (Button) findViewById(R.id.bt_jogar_id);
         textoResultado = (TextView) findViewById(R.id.txt_numero_id);
+        botaoJogar = (Button) findViewById(R.id.bt_jogar_id);
 
-        textoResultado.setText("Texto Alterado");
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random numRandomico = new Random();
+                int numeroAleatorio = numRandomico.nextInt(10);
+                textoResultado.setText("O numero gerado foi:" + numeroAleatorio);
+            }
+        });
+
+
+
     }
 }
